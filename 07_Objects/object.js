418328ab5d5b3f3397f689400c1c3a6d6f48dd1c
elijahchen <code.elijahchen@gmail.com>
2017-02-19 17:21:28 -0500

    - Added examples and references for object creation in JavaScript

---
+++ 07_Objects/object.js
@@ -0,0 +1,54 @@
+//OBJECTS CAN HOLD ALL SORTS OF DATA
+var junkObject = {
+	age: 57,
+	color: "purple",
+	isHungry: true,
+	friends: ["Romeo", "Juliet"];
+	pet: {
+		name: "Goldie",
+		species: "Dog",
+		age: 2
+	}
+};
+
+//Bracket notation, similar to arrays:
+console.log(person["name"]);
+//Dot notation
+console.log(person.name);
+
+//**** RETRIEVING DATA ****
+//You cannot use dot notation if the property starts with a number
+someObject.1blah //Invalid
+someObject["1blah"] //Valid
+
+//You can lookup using a variable with bracket notation
+var str = "name";
+someObject.str //Doesn't look for "name"
+someObject[str] //Does evaluate str and looks for "name"
+
+//You cannot use dot notation for property names with spaces
+someObject.fav color //Invalid
+someObject["fav color"]; //Valid
+
+//Accessing and modifying a parameter within an object
+person.age += 1;
+
+
+//**** CREATING OBJECTS ****
+// Make an empty object and then add to it
+var person = {
+	person.name = "Travis";
+	person.age= 21;
+	person.city = "LA";
+};
+// All at once
+var person1 = {
+	name: "Travis",
+	age: 21,
+	city:"LA"
+};
+// Another way of initializing an object
+ver person = new Object();
+person.name = "Travis";
+person.age = 21;
+person.city = "LA";
