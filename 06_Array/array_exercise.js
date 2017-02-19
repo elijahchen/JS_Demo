bf119d87c156189ea17cf8f257642c5673ff350f
elijahchen <code.elijahchen@gmail.com>
2017-02-19 16:08:33 -0500

    - Refactored and debugged logic

--- 06_Array/array_exercise.js
+++ 06_Array/array_exercise.js
@@ -1,44 +1,38 @@
-console.log("CONNECTED")
-var arr = ["A", "B", "C", "D"];
-
 //Print reverse
 //Write a function that takes an array as an argument and prints out the elements in the array in reverse order.
-function reverse(){
-	for(var i = arr.length()-1; i > 0; i--){
+function printReverse(arr){
+	for(var i = arr.length - 1; i >= 0; i--){
 		console.log(arr[i]);
 	}	
 }
 
 //isUniform()
 //Write a function is Uniform() which takes an array as an argument and returns true if all elements in the array are identical
-function isUniform(){
+function isUniform(arr){
 	var comparedValue = arr[0];
-	var yes;
-	  for(var i = 1; i < arr.length()-1; i++){
-	  	if(arr[i] == comparedValue){
-	  		comparedValue = arr[i];
-	  	} else {
-	  		return yes = false;
+	  for(var i = 1; i < arr.length; i++){
+	  	if(arr[i] !== comparedValue){
+	  		return false;
 	  	}
 	  }
-	  return yes = true;
+	  return true;
 }
 
 //sumArray()
 //Write a function sumArray() that accepts an array of numbers and returns the sum of all numbers in the array
-function sumArray(numbers){
+function sumArray(arr){
 	var sum = 0;
 	arr.forEach(function(index){
-		index+=sum;
+		sum+=index;
 	});
 	return sum;
 }
 
 //max()
 //Write a function max() that accepts an array of numbers and returns the maximum number in the array
-function max(numbers){
-	var high;
-	for(var i = 0; i < arr.length(); i++){
+function max(arr){
+	var high = arr[0];
+	for(var i = 0; i < arr.length; i++){
 		if(arr[i] > high){
 			high = arr[i];
 		}
