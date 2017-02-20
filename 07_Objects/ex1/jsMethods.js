fa4090864fce64b9b6fc70c85f8acb1973689d84
elijahchen <code.elijahchen@gmail.com>
2017-02-19 22:38:24 -0500

    - Adding methods to object, namespacing, and keywords

---
+++ 07_Objects/ex1/jsMethods.js
@@ -0,0 +1,19 @@
+var obj = {
+	name: "Chuck",
+	age: 45,
+	isCool: false,
+	friends: ["bob", "tina"],
+	add: function(x,y){
+		return x + y;
+	}
+}
+
+var dogSpace = {};
+dogSpace.speak = function(){
+	return "WOOF";
+}
+
+var catSpace = {};
+catSpace.speak = function(){
+	return "MEOW";
+}
