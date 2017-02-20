9d5d0fd96c7bcb527d248fc41f9304e663ff76b8
elijahchen <code.elijahchen@gmail.com>
2017-02-20 14:38:24 -0500

    - Disabled corresponding JS dependancy

--- 08_Select/select.js
+++ 08_Select/select.js
@@ -1,22 +1,19 @@
 //METHODS
 //The document comes with a bunch of methods for selecting elements.
-//document.getElementsById()
-//document.getElementsByClassName()
-//document.getElementsByTagName()
+//document.getElementById()
+//document.getElementByClassName()
+//document.getElementByTagName()
 //document.querySelector()
 //document.querySelectorAll()
 
-var h1 = document.querySelector("h1");
-h1.style.color = "pink";
+// var body = document.querySelector("body");
+// var isBlue = false;
 
-var body = document.querySelector("body");
-var isBlue = false;
-
-setInterval(function(){
-	if(isBlue) {
-		body.style.background = "white";
-	} else {
-		body.style.background = "#3498db";
-	}
-	isBlue = !isBlue;
-}, 1000);
+// setInterval(function(){
+// 	if(isBlue) {
+// 		body.style.background = "white";
+// 	} else {
+// 		body.style.background = "#3498db";
+// 	}
+// 	isBlue = !isBlue;
+// }, 1000);
